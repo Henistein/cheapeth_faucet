541e49e0a5f9331fbc40880f6aaddf6c8ebb8c17
Henistein <henriquejesus20@gmail.com>
2022-02-09 01:08:03 +0000

    add cooldown system

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import json
+import time
 from flask import Flask, request
 from web3 import Web3, HTTPProvider
+
+# cooldowns
+class Cooldowns:
+  def __init__(self, path):
+    self.path = path
+    try:
+      self.addresses = json.load(open(path))
+    except:
+      self.addresses = {}
+    self.period = 604800 # 7 days
+
+  def update_file(self):
+    f = open(self.path, 'w')
+    f.write(json.dumps(self.addresses))
+    f.close()
+    
+C = Cooldowns('cooldowns.json')
 
 # load json config
 data = json.load(open('config.json'))
@@ -27,12 +45,22 @@ def index():
 @app.route('/faucet')
 def faucet():
   address = Web3.toChecksumAddress(request.args.get('address'))
-  nonce = w3.eth.getTransactionCount(default_account)
-  tx_hash = contract.functions.sendEther(address).buildTransaction({'chainId':777, 'gas': 300000,'gasPrice': w3.toWei('1', 'gwei'),'nonce': nonce,})
-  signed_txn = w3.eth.account.sign_transaction(tx_hash, private_key=data['private_key'])
-  w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-  return 'sent!'
+  # check if address is on cooldown
+  if (address in C.addresses) and ((C.addresses[address] + C.period) > time.time()):
+    return f"{address} still in cooldown"
+  else:
+    # send cheapeth
+    nonce = w3.eth.getTransactionCount(default_account)
+    tx_hash = contract.functions.sendEther(address).buildTransaction({'chainId':777, 'gas': 300000,'gasPrice': w3.toWei('1', 'gwei'),'nonce': nonce,})
+    signed_txn = w3.eth.account.sign_transaction(tx_hash, private_key=data['private_key'])
+    w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    
+    # update cooldowns
+    C.addresses[address] = time.time()
+    C.update_file()
+
+    return 'sent!'
 
 if __name__ == '__main__':
 
